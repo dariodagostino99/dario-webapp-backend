4d280e37c37c16ab09214e635a7028d8fb8575d4
dariodagostino99 <77460800+dariodagostino99@users.noreply.github.com>
2022-02-02 19:24:38 -0300

    Added cors origin to all methods, but need to do it in a more scalable way

--- src/main/java/com/dario/webapp/backend/demo/TaskController.java
+++ src/main/java/com/dario/webapp/backend/demo/TaskController.java
@@ -19,17 +19,20 @@ public class TaskController {
     }
 
     @PostMapping("")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Task createTask(@RequestBody TaskDTO taskDTO){
         return taskService.createTask(taskDTO);
     }
 
     @PutMapping("/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Task updateTask(@PathVariable("id") Long taskId, @RequestBody TaskDTO taskDTO) {
         return taskService.updateTask(taskId, taskDTO);
     }
 
     @DeleteMapping("/{id}")
-    public Task updateTask(@PathVariable("id") Long taskId) {
+    @CrossOrigin(origins = "http://localhost:3000")
+    public Task deleteTask(@PathVariable("id") Long taskId) {
         return taskService.deleteTask(taskId);
     }
 
