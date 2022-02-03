d54ef42f93bf3ae3a27b23190067ef2034161869
dariodagostino99 <77460800+dariodagostino99@users.noreply.github.com>
2022-02-02 23:42:21 -0300

    Added more scalable cors policy

--- src/main/java/com/dario/webapp/backend/demo/DemoApplication.java
+++ src/main/java/com/dario/webapp/backend/demo/DemoApplication.java
@@ -3,8 +3,11 @@ package com.dario.webapp.backend.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -12,6 +15,18 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("OPTIONS", "GET", "POST", "PUT", "PATCH" ,"DELETE")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- src/main/java/com/dario/webapp/backend/demo/TaskController.java
+++ src/main/java/com/dario/webapp/backend/demo/TaskController.java
@@ -13,25 +13,21 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping("")
-    @CrossOrigin(origins = "http://localhost:3000")
     public List<Task> getAllTasks(){
         return taskService.getAllTasks();
     }
 
     @PostMapping("")
-    @CrossOrigin(origins = "http://localhost:3000")
     public Task createTask(@RequestBody TaskDTO taskDTO){
         return taskService.createTask(taskDTO);
     }
 
     @PutMapping("/{id}")
-    @CrossOrigin(origins = "http://localhost:3000")
     public Task updateTask(@PathVariable("id") Long taskId, @RequestBody TaskDTO taskDTO) {
         return taskService.updateTask(taskId, taskDTO);
     }
 
     @DeleteMapping("/{id}")
-    @CrossOrigin(origins = "http://localhost:3000")
     public Task deleteTask(@PathVariable("id") Long taskId) {
         return taskService.deleteTask(taskId);
     }
